70527569b1b1c1567eb2ef62fbdea42c91dfc0d2
Ganesh Balaji <ganeshkbalaji@ymail.com>
2015-09-21 17:12:43 -0700

    updated html and css for default picture

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,17 @@
+// var colourData;
 $( document ).ready(function() {
-$.ajax({
+  function parseJSON() {
+    alert(globalArr);
+  }
+  var globalArr = new Array();
+  $.ajax({
         url: 'http://www.colourlovers.com/api/palettes?format=json&jsonCallback=callback&',
         dataType: "jsonp",
         jsonpCallback: 'callback',
         success: function(response){
-          console.log(response)
-        }
-  })
+          $.each(response, function(index, value) {
+            globalArr.push(value)
+            alert(globalArr);
+          })
+  });
 });
